26a9fba461e064a95d83c335702823ca4899af61
JelenaIlicAlimpijevic <ma1jelena@yahoo.com>
2017-10-19 21:56:51 +0200

    Treci zadatak nepotpun

---
+++ src/app/shared/exsamples.ts
@@ -0,0 +1,32 @@
+export class Examples {
+	private examples = [
+    {
+      id: 1,
+      name: "It",  
+      director: "Andy Muschietti",
+      imageUrl: "http://www.imdb.com/title/tt1396484/mediaviewer/rm1002251008",
+      duration: 135,
+      releaseDate: "8 September 2017",
+      genres: ["horror", "thriller", "drama"]
+    },
+    {
+      id: 2,
+      name: "Bates Motel",  
+      director: "Anthony Cipriano",
+      imageUrl: "http://www.imdb.com/title/tt2188671/mediaviewer/rm1469005312",
+      duration: 45,
+      releaseDate: "18 March 2013",
+      genres: ["horror", "thriller", "drama", "mystery"] 
+    },
+    {
+      id: 3,
+      name: "Black Swan",  
+      director: "Darren Aronofsky",
+      imageUrl: "http://www.imdb.com/title/tt0947798/mediaviewer/rm1503101184",
+      duration: 108,
+      releaseDate: "17 December 2010",
+      genres: ["thriller", "drama"]
+    }
+   
+  ];]
+}
--- src/app/shared/services/movies.service.ts
+++ src/app/shared/services/movies.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
 import {Movie} from '../models/movie';
+import {Examples} from '../exsamples';
+import {Observable, Observer} from 'rxjs';
+
 
 @Injectable()
 export class MoviesService {
+	private movies:Movie[];
 
-  constructor() { }
+	getMovies(examples:Examples){
+		
+	return new Observable((o:Observer<any>)=>{
+   	  const movie=examples.map(example=>new Movie(example.id, 
+   	  												example.name, 
+   	  												example.director,
+   	  												example.imageUrl,
+   	  												example.duration,
+   	  												example.releaseDate,
+   	  												example.genres
+												));
+      o.next(movie);
+  });
+}
+  
 
 }
